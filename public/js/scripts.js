2d1d13ecebf7fedf8bd34bee25202e40bd11e298
Ryan Wood <Ryan.Wood01@bbc.co.uk>
2020-11-22 12:30:51 +0000

    list all products in the web UI

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,6 +1,5 @@
 loadSingleProduct = function(type) {
-    $.get(`product/${type}`, function( product_data ) {
-        console.log(`#${type}`);
+    $.get(`product/${type}`, function(product_data) {
         $(`#${type}_product`).prepend(`<img src="${product_data.image}">`);
         $(`#${type}_product .name`).text(product_data.name);
         $(`#${type}_product .style`).text(product_data.style);
@@ -8,7 +7,27 @@ loadSingleProduct = function(type) {
     });
 };
 
+productsListTableRow = function(product_data) {
+    return `
+    <tr>
+        <td><img src="${product_data.image}"></td>
+        <td>${product_data.name}</td>
+        <td>${product_data.style}</td>
+        <td>${product_data.price}</td>
+    </tr>
+    `
+};
+
+loadProductsList = function() {
+    $.get('products', function(products_list) {
+        products_list.forEach(function(product) {
+            $("#products_list table tbody").append(productsListTableRow(product));
+        });
+    });
+};
+
 $( document ).ready(function(){
     loadSingleProduct("cheapest");
     loadSingleProduct("most_expensive");
+    loadProductsList();
 });
